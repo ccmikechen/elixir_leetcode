ae89c09501b212d74f0e4723b60eb753f7199128
Mike Chen <ccmikechen@gmail.com>
2019-08-27 04:24:00 +0800

    Initial commit

---
+++ lib/leet_code.ex
@@ -0,0 +1,18 @@
+defmodule LeetCode do
+  @moduledoc """
+  Documentation for LeetCode.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LeetCode.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/leet_code_test.exs
@@ -0,0 +1,8 @@
+defmodule LeetCodeTest do
+  use ExUnit.Case
+  doctest LeetCode
+
+  test "greets the world" do
+    assert LeetCode.hello() == :world
+  end
+end
